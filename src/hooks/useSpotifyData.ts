5132e89e38ef2dd6f1f1d1b2bf750ec6d2c5994e
dahiyasahil44 <imsahil440407@gmail.com>
2025-12-15 23:32:18 +0530

    Add Spotify tracks dataset and define TypeScript models and Implement Spotify data loading with CSV parsing and error handling

---
+++ src/hooks/useSpotifyData.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import Papa from "papaparse";
+import type { SpotifyTrack } from "../types/spotify";
+
+
+export function useSpotifyData() {
+    const [data, setData] = useState<SpotifyTrack[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string>("");
+
+    useEffect(()=>{
+        Papa.parse("/spotify_songs.csv", {
+            download: true,
+            header: true,
+            delimiter: ",",
+            skipEmptyLines: true,
+            dynamicTyping: true,
+            complete: (result) => {
+                // console.log("Parsed data:", result.data);
+                setData(result.data as SpotifyTrack[]);
+                setLoading(false);
+            },
+            error: (err) => {
+                console.error(err);
+                setError("Failed to parse Spotify dataset");
+                setLoading(false);
+            },
+        });
+
+
+    },[]);
+
+    return { data, loading, error };
+}
---
+++ src/types/spotify.ts
@@ -0,0 +1,13 @@
+export interface SpotifyTrack{
+    track_name: string,
+    artist_name: string,
+    album_name: string,
+    genre: string,
+    popularity: number,
+    tempo: number,
+    energy: number,
+    danceability: number,
+    duration_ms: number,
+    release_date: string,
+    explicit: boolean
+}
